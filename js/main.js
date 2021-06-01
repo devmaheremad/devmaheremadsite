7d7d567df445ebf548db5661c6ec956395e3d138
Maher E. Elmashharawi <mmashharawi96@students.iugaza.edu.ps>
2021-06-01 15:20:07 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -143,7 +143,7 @@
     $('.back-to-top').click(function() {
         $('html, body').animate({
             scrollTop: 0
-        }, 1500, 'easeInOutExpo');
+        }, 500, 'easeInOutExpo');
         return false;
     });
 
